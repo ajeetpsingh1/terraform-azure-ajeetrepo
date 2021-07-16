902a8e52884f2ceb3c38316f902e55246ed65cdc
ajeetpsingh1 <87511146+ajeetpsingh1@users.noreply.github.com>
2021-07-16 11:00:04 +0530

    Update stroage.tf

--- modules/storageaccount/stroage.tf
+++ modules/storageaccount/stroage.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "akilarg1" {
+resource "azurerm_resource_group" "ajeetg1" {
   name = var.resourcegroupname
   location = var.resourcegplocation
 }
 
-resource "azurerm_storage_account" "akilastorage" {
+resource "azurerm_storage_account" "ajeetstorage" {
   name = var.storageaccountname
   resource_group_name = azurerm_resource_group.akilarg1.name
-  location = azurerm_resource_group.akilarg1.location
+  location = azurerm_resource_group.ajeetg1.location
   access_tier = var.tier 
   account_replication_type = "LRS"
   account_tier = "Standard"
